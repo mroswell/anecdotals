346c8e054771a7f2ba859c79be1dfd262f9d8822
Marjorie Roswell <mroswell@gmail.com>
2022-10-02 17:31:43 -0400

    remove extraneous js libraries

--- themes/thomson-hugo/assets/js/script.js
+++ themes/thomson-hugo/assets/js/script.js
@@ -2,46 +2,46 @@
 $(window).on('load', function () {
   $('.preloader').fadeOut(100);
 });
-
-(function ($) {
-
-  // carosel
-  $('.owl-carousel').owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay: true,
-    dots: false,
-    autoplayTimeout: 8000
-  });
-
-  // Shuffle js filter and masonry
-  var containerEl = document.querySelector('.shuffle-wrapper');
-  if (containerEl) {
-    var Shuffle = window.Shuffle;
-    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-      itemSelector: '.shuffle-item',
-      buffer: 1
-    });
-
-    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-      var input = evt.currentTarget;
-      if (input.checked) {
-        myShuffle.filter(input.value);
-      }
-    });
-  }
-
-  // magnific popup
-  $('.project-gallery').each(function () {
-    $(this).find('.popup-gallery').magnificPopup({
-      type: 'image',
-      gallery: {
-        enabled: true
-      }
-    });
-  });
-
-
-
-
-})(jQuery);
+//
+// (function ($) {
+//
+//   // carosel
+//   $('.owl-carousel').owlCarousel({
+//     items: 1,
+//     loop: true,
+//     autoplay: true,
+//     dots: false,
+//     autoplayTimeout: 8000
+//   });
+//
+//   // Shuffle js filter and masonry
+//   var containerEl = document.querySelector('.shuffle-wrapper');
+//   if (containerEl) {
+//     var Shuffle = window.Shuffle;
+//     var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+//       itemSelector: '.shuffle-item',
+//       buffer: 1
+//     });
+//
+//     jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+//       var input = evt.currentTarget;
+//       if (input.checked) {
+//         myShuffle.filter(input.value);
+//       }
+//     });
+//   }
+//
+//   // magnific popup
+//   $('.project-gallery').each(function () {
+//     $(this).find('.popup-gallery').magnificPopup({
+//       type: 'image',
+//       gallery: {
+//         enabled: true
+//       }
+//     });
+//   });
+//
+//
+//
+//
+// })(jQuery);
